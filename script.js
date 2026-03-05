aa8bf8a5e07900c7128e7c38e67a7682091d280c
ElisaEscola <elisabeth.martins@escola.pr.gov.br>
2026-03-05 18:19:11 -0300

    Exercício pronto com as requisitos atendidos

---
+++ script.js
@@ -0,0 +1,10 @@
+const botaoFoda = document.getElementById("botaoFoda");
+const editar = document.getElementById("editar");
+
+const nome = document.querySelector(".nome");
+const descricao = document.querySelector(".descricao");
+
+botaoFoda.addEventListener("click", function() {
+  alert("O meu nome é " + nome.textContent);
+});
+
